fa28ce59e9fa044c2f13c19fbb07148ca05e641b
Cau-Correa <caupontocorrea@gmail.com>
2021-02-02 16:39:44 +0000

    Add background image

--- script.js
+++ script.js
@@ -1,14 +1,5 @@
-    const fetch = require('node-fetch');
-
-    (async () => {
-    const response = await fetch('https://trefle.io/api/v1/plants?token=ZWEVXNJFwXLOVFaAzlGwesIAAC8Y-F6dRuQ1M1cUo8Q&page=2');
-    const json = await response.json();
-    console.log(json);
-    })();
-
-
 function plantInformationHTML(plants) {
-    return `
+  return `
         <h2>${plants.name}
             <span>
                 (<a href="${plants.html_url}" target="_blank">${plants}</a>)
@@ -17,32 +8,33 @@ function plantInformationHTML(plants) {
 }
 
 
-function fetchTrefleInformation(event){
-    var plantname = $("#plant-name").val();
-    if (!plantname){
-        $("#tf-plants-data").html(`<h3>Please enter a plant name</h3>`);
-        return;
-    }
-        $("#tf-plants-data").html(
-            `<div id="loader">
+function fetchTrefleInformation(event) {
+  var plantname = $("#plant-name").val();
+  if (!plantname) {
+    $("#tf-plants-data").html(`<h3>Please enter a plant name</h3>`);
+    return;
+  }
+  $("#tf-plants-data").html(
+    `<div id="loader">
             <img src="assets/loader.gif" alt="loading..." />
         </div>`);
 
 
-$.when(
+  $.when(
     $.getJSON('https://trefle.io/api/v1/plants?token=ZWEVXNJFwXLOVFaAzlGwesIAAC8Y-F6dRuQ1M1cUo8Q&page=2/${plantname}')
-).then(
+  ).then(
     function(response) {
-        var plantData = response;
-        $(`#tf-plants-data`).html(plantInformationHTML(plantData));
-    }, function(errorResponse) {
-        if (errorResponse.status === 404) {
-            $(`#tf-plants-data`).html(
-                `<h3>No info found for plant ${plantname}</h3>`)
-        } else {
-            console.log(errorResponse);
-            $(`#tf-plant-data`).html(
-                `<h3>Error:${errorResponse.responseJSON.message}</h3>`);
-        }
+      var plantData = response;
+      $(`#tf-plants-data`).html(plantInformationHTML(plantData));
+    },
+    function(errorResponse) {
+      if (errorResponse.status === 404) {
+        $(`#tf-plants-data`).html(
+          `<h3>No info found for plant ${plantname}</h3>`)
+      } else {
+        console.log(errorResponse);
+        $(`#tf-plant-data`).html(
+          `<h3>Error:${errorResponse.responseJSON.message}</h3>`);
+      }
     });
 }
